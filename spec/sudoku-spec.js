4382bc0039ffee188411a1d7bf46123ba7e88fc5
Daniel Burbach and Phil Mass <phil@>
2019-01-08 14:41:52 -0800

    add row, column, and box, and tests for them

--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -1,13 +1,37 @@
 import { Sudoku } from './../src/sudoku';
 
-describe('Sudoku', function() {
-  // let roman;
+describe('Sudoku sections', function() {
+  let sudoku;
+  beforeEach(function() {
+    sudoku = new Sudoku();
+    sudoku.setValues(Array.from(Array(81), (val, index) => index + 1));
+  });
 
+  it('should return a row', function() {
+    expect(sudoku.getRow(5)).toEqual([46,47,48,49,50,51,52,53,54]);
+  });
+  it('should return a column', function() {
+    expect(sudoku.getColumn(3)).toEqual([4,13,22,31,40,49,58,67,76]);
+  });
+  it('should return a box', function() {
+    expect(sudoku.getBox(4)).toEqual([31,32,33,40,41,42,49,50,51]);
+  });
+});
+
+describe('Sudoku values', function() {
+  let sudoku;
   // beforeEach(function() {
-  //   roman = new Roman();
+  //   sudoku = new Sudoku();
+  //sudoku.setValues([8,2,5,4,7,1,3,9,6,1,9,4,3,2,6,5,7,8,3,7,6,9,8,5,2,4,1,5,1,9,7,4,3,8,6,2,6,3,2,5,9,8,4,1,7,4,8,7,6,1,2,9,3,5,2,6,3,1,5,9,7,8,4,9,4,8,2,6,7,1,5,3,7,5,1,8,3,4,6,2,9]);
   // });
-
-  //  it('should convert 1 to I', function() {
-  //   expect(roman.romanize(1)).toEqual("I");
+  //
+  // it('should return a row', function() {
+  //   expect(sudoku.getRow(5)).toEqual([46,47,48,49,50,51,52,53,54]);
+  // });
+  // it('should return a column', function() {
+  //   expect(sudoku.getColumn(3)).toEqual([4,13,22,31,40,49,58,67,76]);
+  // });
+  // it('should return a box', function() {
+  //   expect(sudoku.getBox(4)).toEqual([31,32,33,40,41,42,49,50,51]);
   // });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,7 +1,36 @@
 class Sudoku {
   constructor() {
-    let values = Array(81).keys();
-    debugger;
+    this.values = [];
+    this.values.fill(0);
+  }
+
+  setValues(values) {
+    this.values = values;
+  }
+
+  getRow(index) {
+    if(index >= 0 && index <= 8) {
+      return this.values.slice(9 * index, 9 * (index + 1));
+    }
+  }
+
+  getColumn(index) {
+    return this.values.filter((value, i) => i % 9 === index);
+  }
+
+  getBox(index) {
+    const topLefts = [0, 3, 6, 27, 30, 33, 54, 57, 60];
+    let values = [];
+    let rowIndex = topLefts[index];
+    for(let i = 0; i < 3; i++) {
+      values = values.concat(this.values.slice(rowIndex, rowIndex + 3));
+      rowIndex += 9;
+    }
+    return values;
+  }
+
+  verifySection(section) {
+    return section.length == 9;
   }
 }
 
